e61e4ae6213867e97276ae0f382af5865e55aa08
João Ricardo <joao.luiz.ric@gmail.com>
2025-07-12 13:13:56 -0300

    Day-17: Validate Forms

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,10 @@ Route::get('/job/{id}', function ($id) {
 });
 
 Route::post('/jobs', function() {
-    // validation...
+    request()->validate([
+        'title' => ['required', 'min:3', 'max:100'],
+        'salary' => ['required']
+    ]);
 
     Job::create([
         'title' => request('title'),
