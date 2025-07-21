c20410fb374662a9b915b0907b6206febc8b41ba
João Ricardo <joao.luiz.ric@gmail.com>
2025-07-20 21:01:36 -0300

    Day-24 and 25: Queue, Job and Worker

--- resources/views/mail/job-posted.blade.php
+++ resources/views/mail/job-posted.blade.php
@@ -1,30 +1,8 @@
-@vite('resources/css/app.css')
-<div>
-    <div class="px-4 sm:px-0">
-        <h3 class="text-base/7 font-semibold text-gray-900">Sistema automático de notificações</h3>
-        <p class="mt-1 max-w-2xl text-sm/6 text-gray-500">Vaga publicada com sucesso!</p>
-    </div>
-    <div class="mt-6 border-t border-gray-100">
-        <dl class="divide-y divide-gray-100">
-            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                <dt class="text-sm/6 font-medium text-gray-900">Employer</dt>
-                <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">{{ $job->employer->name }}</dd>
-            </div>
-            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                <dt class="text-sm/6 font-medium text-gray-900">Application for</dt>
-                <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">{{ $job->title }}</dd>
-            </div>
-            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                <dt class="text-sm/6 font-medium text-gray-900">Salary expectation</dt>
-                <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">{{ $job->salary }}</dd>
-            </div>
-            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                <dt class="text-sm/6 font-medium text-gray-900">About</dt>
-                <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">Fugiat ipsum ipsum deserunt culpa aute
-                    sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint.
-                    Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad
-                    adipisicing reprehenderit deserunt qui eu.</dd>
-            </div>
-        </dl>
-    </div>
-</div>
+<h2>{{ $job->title }}</h2>
+<p>
+    Parabéns sua vaga foi postada no portal de empregos!
+</p>
+
+<p>
+    <a href="{{ url('/jobs/' . $job->id) }}">Veja sua vaga publicada.</a>
+</p>
